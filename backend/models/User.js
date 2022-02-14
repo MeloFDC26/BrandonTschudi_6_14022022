ce8d328407098aa49f35ef515617b7cb29cd14fc
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-02-14 17:44:12 +0100

    Création de la base de données modèle 'utilisateur' (User.js dans /models/)

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,2 +1,17 @@
 //Création de la base de données modèle utilisateur
+const mongoose = require('mongoose');
 
+const userSchema = mongoose.schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+});
+
+// Importation du module empechant la création de deux comptes avec le même email
+const uniqueMailValidator = require('mongoose-unique-validator');
+
+//Appel du module empechant la création de deux comptes avec le même email
+//pour le modèle 'userSchema'
+userSchema.plugin(uniqueMailValidator);
+
+//Export de la base créée (objet créé ici)
+module.exports = mongoose.model('User', userSchema);
