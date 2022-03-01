2e2b7b2ae5d3f766f51f3d824113f6a8c831ef6f
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-01 17:00:01 +0100

    Création du middleware permettant à TOUS les utilisateurs d'utiliser les routes définies

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,20 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+//Middleware pour permettre à tout le monde d'utiliser les routes ci-dessous
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(bodyParser.json());
 
