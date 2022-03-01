82786e069cbbcbca5ca6e57f1d3d51c6f0bf0b90
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-01 17:12:00 +0100

    Déplacement importation de mongoose-unique-validator

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,13 @@
 //Création de la base de données modèle utilisateur
 const mongoose = require('mongoose');
 
+// Importation du module empechant la création de deux comptes avec le même email
+const uniqueMailValidator = require('mongoose-unique-validator');
+
 const userSchema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
-
-// Importation du module empechant la création de deux comptes avec le même email
-const uniqueMailValidator = require('mongoose-unique-validator');
 
 //Appel du module empechant la création de deux comptes avec le même email
 //pour le modèle 'userSchema'
