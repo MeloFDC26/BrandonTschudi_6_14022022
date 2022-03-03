ac80ca98b9939e6acde69104aa4d84fb86762157
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-03 14:58:00 +0100

    Ajout du middleware multer à la route POST sauces

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 router.get('/', auth, saucesCtrl.getAllSauces);
 router.get("/:id", auth, saucesCtrl.getOneSauce);
-router.post('/:id', auth, saucesCtrl.updateOneSauce);
+router.post('/:id', auth, multer, saucesCtrl.updateOneSauce);
 
 
 module.exports = router;
