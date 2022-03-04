9a6317acd1dd12c97094bf935171c8ac0bbca1ba
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-04 14:41:46 +0100

    Modification du temps de validité du token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -37,7 +37,7 @@ exports.login = (req, res, next) => {
             token: jwt.sign(
                 { userID: user._id },
                 'RANDOM_TOKEN_SECRET',
-                { expiresIn: '24h' }
+                { expiresIn: '100h' }
             )
           });
         })
