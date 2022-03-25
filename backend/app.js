7962d5398fcaa62023801281a7246141cd4da7fd
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-25 14:55:34 +0100

    console.log pour 'dotenv'

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,14 @@ const userRoutes = require('./routes/user');
 const saucesRoutes = require('./routes/sauces');
 
 //Connection à la base de données (MONGOOSE = noSQL) et protection des données sensibles avec 'dotenv'
+console.log(process.env);
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
+  .catch(console.error);
 
 //Middleware pour permettre à tout le monde d'utiliser les routes ci-dessous
 app.use((req, res, next) => {
